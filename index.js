db30f5224c97e59562c6f53d15e3d02fcc71b4b9
Prodigle <main.prodigle@gmail.com>
2018-06-28 21:38:54 +0100

    Removed submodule templates/Jinko/default

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const createCmd = require('./lib/create');
 prog
     .version('1.0.0')
     .command('create', 'Create a new application')
-    .argument('<template>', 'Template to use')
-    .option('--variant <variant>', 'Which <variant> of the template is going to be created')
     .action(createCmd);
 
 prog.parse(process.argv);
